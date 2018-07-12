b7afab53867e09b2ebf46981e770470d1cb7a3d5
Kira Hartness <kirahartness@gmail.com>
2018-07-12 13:38:36 +0000

    automatically backed up by learn

--- statecapital.rb
+++ statecapital.rb
@@ -15,6 +15,7 @@ states = {"Massachusetts" => "MA",
 
 #     Level 1: Write some code which given a state name ("Massachusetts") outputs its capital ("Boston")
 #     Level 2: Handle the case when a state's information is not known by returning "Unknown"
+
 #     Level 3: Now let's go the other way. Given a capital name ("Denver"), return the state name for which it is the capital ("Colorado")
 
 states = {"Massachusetts" => "MA",
