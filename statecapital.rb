019689a30bde4d5a67526c3243716afa0ada6db4
Kira Hartness <kirahartness@gmail.com>
2018-07-12 14:01:32 +0000

    automatically backed up by learn

--- statecapital.rb
+++ statecapital.rb
@@ -36,29 +36,6 @@
 states = {"Massachusetts" => "MA",
           "Wisconsin" => "WI",
           "New Jersey" => "NJ",
-          "Colorado" => "CO"}
-
-capitals = {"MA" => "Boston",
-            "WI" => "Madison",
-			      "NJ" => "Trenton",
-	 		      "CO" => "Denver"}.reverse_each
-states = {"Massachusetts" => "MA",
-          "Wisconsin" => "WI",
-          "New Jersey" => "NJ",
-          "Colorado" => "CO"}
-
-capitals = {"MA" => "Boston",
-            "WI" => "Madison",
-			      "NJ" => "Trenton",
-	 		      "CO" => "Denver"}
-	 		
-	 	puts "What state do you want to know the capital of?"
-	 	given = gets.chomp
-	 	puts capitals[states[given]]
-	 	
-	 	states = {"Massachusetts" => "MA",
-          "Wisconsin" => "WI",
-          "New Jersey" => "NJ",
           "Colorado" => "CO",
           "New York" => "NY"}
 
