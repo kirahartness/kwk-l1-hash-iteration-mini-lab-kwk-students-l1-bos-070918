d5723cb586a540c650a4ee3ce520f9554154de46
Kira Hartness <kirahartness@gmail.com>
2018-07-12 13:27:56 +0000

    automatically backed up by learn

--- statecapital.rb
+++ statecapital.rb
@@ -3,10 +3,17 @@ states = {"Massachusetts" => "MA",
            "New Jersey" => "NJ",
            "Colorado" => "CO"}
 
-# capitals = {"MA" => "Boston",
-#             "WI" => "Madison",
-#             "NJ" => "Trenton",
-#             "CO" => "Denver"}
+ capitals = {"MA" => "Boston",
+             "WI" => "Madison",
+             "NJ" => "Trenton",
+             "CO" => "Denver"}
+             
+  states.each do |x|
+    iterate = 0
+    states[x]=capital[iterate]
+    iterate += 1 
+  end
+  puts states
 
 #     Level 1: Write some code which given a state name ("Massachusetts") outputs its capital ("Boston")
 #     Level 2: Handle the case when a state's information is not known by returning "Unknown"
@@ -21,6 +28,3 @@ capitals = {"MA" => "Boston",
             "WI" => "Madison",
 			"NJ" => "Trenton",
 	 		"CO" => "Denver"}
-
-
-Add CommentCollapse 
