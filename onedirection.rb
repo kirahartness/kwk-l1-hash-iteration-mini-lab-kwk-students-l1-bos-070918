08c4baa77b5b9bd8fc28fc696331d0b1c420f231
Kira Hartness <kirahartness@gmail.com>
2018-07-12 14:05:31 +0000

    automatically backed up by learn

---
+++ onedirection.rb
@@ -0,0 +1,13 @@
+one_direction = {"Zayn" => 25,
+                 "Liam" => 24,
+                 "Louis" => 26,
+                 "Harry" => 24,
+                 "Niall" => 24 
+}
+
+# How can you print out all of the members and their ages like the following:
+puts  "Zayn is 25 years old."
+puts  "Liam is 24 years old."
+puts  "Louis is 26 years old."
+puts  "Harry is 24 years old."
+puts  "Niall is 24 years old."
