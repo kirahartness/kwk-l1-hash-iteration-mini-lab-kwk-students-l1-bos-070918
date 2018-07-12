bb14741354d0f873d234def04f81e48bff1b3340
Kira Hartness <kirahartness@gmail.com>
2018-07-12 13:58:47 +0000

    automatically backed up by learn

--- statecapital.rb
+++ statecapital.rb
@@ -15,21 +15,21 @@
 
 #     Level 1: Write some code which given a state name ("Massachusetts") outputs its capital ("Boston")
 #     Level 2: Handle the case when a state's information is not known by returning "Unknown"
-states = {"Massachusetts" => "MA", 
-           "Wisconsin" => "WI",
-           "New Jersey" => "NJ",
-           "Colorado" => "CO"}
+#states = {"Massachusetts" => "MA", 
+ #          "Wisconsin" => "WI",
+  #         "New Jersey" => "NJ",
+  #         "Colorado" => "CO"}
 
- capitals = {"MA" => "Boston",
-             "WI" => "Madison",
-             "NJ" => "Trenton",
-             "CO" => "Denver"}
+ #capitals = {"MA" => "Boston",
+ #            "WI" => "Madison",
+ #            "NJ" => "Trenton",
+  #           "CO" => "Denver"}
             
-  given = "North alaska"
-  if capitals[states[given]].nil?
-    puts "Unknown"
-  else puts capitals[states[given]]
-end
+  #given = "North alaska"
+  #if capitals[states[given]].nil?
+    #puts "Unknown"
+  #else puts capitals[states[given]]
+#end
 
 #     Level 3: Now let's go the other way. Given a capital name ("Denver"), return the state name for which it is the capital ("Colorado")
 
