3db46079087be3b6e38901a8b3094eee3d7d58e2
Kira Hartness <kirahartness@gmail.com>
2018-07-12 13:44:01 +0000

    automatically backed up by learn

--- statecapital.rb
+++ statecapital.rb
@@ -52,4 +52,23 @@ capitals = {"MA" => "Boston",
 	 	given = gets.chomp
 	 	puts capitals[states[given]]
 	 	
-	 	
+	 	states = {"Massachusetts" => "MA",
+          "Wisconsin" => "WI",
+          "New Jersey" => "NJ",
+          "Colorado" => "CO",
+          "New York" => "NY"}
+
+capitals = {"MA" => "Boston",
+            "WI" => "Madison",
+            "NJ" => "Trenton",
+            "CO" => "Denver",
+            "NY" => "Albany"}
+
+
+puts "What state would you like to know the capital of?"
+given = gets.chomp
+
+puts capitals[states[given]]
+capital = capitals[states[given]]
+
+puts "The capital of #{given} is #{capital}."
